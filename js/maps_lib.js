f4f6103571c349e99c6e2da724703aa10759fdda
Riccardo Corti <corti.riccardo@gmail.com>
2016-08-26 09:21:14 +0200

    aggiungta icona marker di default

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -37,7 +37,8 @@
                 this.addrMarkerImage = ""
         }
         else
-            this.addrMarkerImage = "images/blue-pushpin.png"
+            //this.addrMarkerImage = "images/blue-pushpin.png"
+            this.addrMarkerImage = "images/home_pinter.png"
 
     	this.currentPinpoint = null;
     	$("#result_count").html("");
